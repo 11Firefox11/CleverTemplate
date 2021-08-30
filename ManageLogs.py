206ac7c45f9dffcf85d7cdcecf896aad396b990d
11Firefox11 <t.viktorvagyok@gmail.com>
2021-08-30 12:38:09 +0200

    Added ManageLogs, little work

---
+++ ManageLogs.py
@@ -0,0 +1,31 @@
+import os, sqlite3, datetime
+
+def connect_databse():
+    conn = sqlite3.connect("./logs.db")
+    return conn
+
+def CreateTable(tabletype):
+    try:
+        tables = {
+            "template":'CREATE TABLE IF NOT EXISTS templates (time TEXT,frompath TEXT,topath TEXT);'
+        }
+        conn = connect_databse()
+        c = conn.cursor()
+        c.execute(tables[tabletype])
+        c.close()
+        return True
+    except:
+        return False
+
+def LogTemplate(frompath, topath, templatetime=None):
+    conn = connect_databse()
+    if templatetime == None:
+        templatetime = str(datetime.datetime.now().strftime(r"%Y-%m-%d %H:%M:%S"))
+    if conn:
+        CreateTable("template")
+        c = conn.cursor()
+        insertcommand = 'INSERT INTO templates(time, frompath, topath) VALUES(?,?,?)'
+        c.execute(insertcommand, (templatetime, frompath, topath))
+        conn.commit()
+        print("x")
+        c.close()
